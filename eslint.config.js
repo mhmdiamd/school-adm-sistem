bfc3c4418e3112b50139acf586a8d5e8cccf691c
mhmdiamd <m1pro2021@mhmdiamd-2.local>
2025-02-06 14:32:56 +0700

    ci: implement husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,15 @@
+const { FlatCompat } = require('@eslint/eslintrc');
 const js = require('@eslint/js');
 const ts = require('@typescript-eslint/eslint-plugin');
 const tsParser = require('@typescript-eslint/parser');
 const prettier = require('eslint-plugin-prettier');
 const unusedImports = require('eslint-plugin-unused-imports');
 
-// const compat = new FlatCompat();
+const compat = new FlatCompat();
 
 module.exports = [
   js.configs.recommended,
-  ts.configs.recommended,
+  ...compat.config(ts.configs.recommended),
   {
     files: ['src/**/*.ts'],
     languageOptions: {
